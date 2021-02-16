bf71224a21b322cb719aa81c44ec1f7b46145c0a
gar17560 <45210913+gar17560@users.noreply.github.com>
2021-02-16 17:15:53 -0600

    Motores DC
    se agregó el control de velocidad y dirección de los motores DC

--- Completo/Completo.ino
+++ Completo/Completo.ino
@@ -12,12 +12,16 @@
 
 Servo servo1;
 Servo servo2;
+AF_DCMotor motor4(4);
+AF_DCMotor motor3(3);
 AF_Stepper motor1(200, 1);
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
 //--------------------------- Variables ---------------------------------------------------
-int recibir[]={0,90,0,0};
+int recibir[]={0,90,0,0,0};
 int contador = 0;
+int vel_dc = 0;
+int memoria_dc = 0;
 
 //--------------------------- Funciones ---------------------------------------------------
 void forwardstep1() {   // you can change these to DOUBLE or INTERLEAVE or MICROSTEP!
@@ -48,22 +52,55 @@ void loop() {
  if(Serial.available()>3){      // recibimos los datos del serial y los guardamos en el array
    if(Serial.read()==0xFF){
        recibir[1] = Serial.read(), recibir[2] = Serial.read(); recibir[3] = Serial.read();
+       recibir[4] = Serial.read();
+       recibir[5] = Serial.read();
        Serial.println(mlx.readAmbientTempC());
        Serial.println(mlx.readObjectTempC());
-       Serial.flush();
+       
+       // stepper
        recibir[3] = map(recibir[3],0,200,-2000,2000);
        stepper1.moveTo(recibir[3]);
+       // DC
+       recibir[5] = map(recibir[5],30,100,140,255);
+       if (recibir[4] == 1){
+        if (memoria_dc == 3){
+          motor4.run(RELEASE);
+          motor3.run(RELEASE);
+          delay(200);
+        }
+        if (recibir[4] == 1){
+          motor4.setSpeed(recibir[5]);
+          motor3.setSpeed(recibir[5]);
+          motor4.run(FORWARD);
+          motor3.run(FORWARD);
+          memoria_dc = 1;
+        }
+
+       }
+       else if (recibir[4] == 2){
+        motor4.run(RELEASE);
+        motor3.run(RELEASE);
+        delay(200);
+       }
+       else if (recibir[4] == 3){
+        if (memoria_dc == 1){
+          motor4.run(RELEASE);
+          motor3.run(RELEASE);
+          delay(200);
+        }
+        if (recibir[4] == 3){
+          motor4.run(BACKWARD);
+          motor3.run(BACKWARD); 
+          motor4.setSpeed(recibir[5]);
+          motor3.setSpeed(recibir[5]);
+          memoria_dc = 3;
+        }
+       }
+       
+       Serial.flush();
      }
  }
- else{
-     if(contador == 100){
-      //Serial.print(temp_obj);
-      //Serial.print(temp_amb);
-  //    Serial.println(255);
 
-      contador=0;
-    }
- }
   servo1.write(recibir[1]);
   servo2.write(recibir[2]);
   stepper1.run();
